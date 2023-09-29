b3d4e9d9b2fca204c5105c224ccd42c0d2c5a861
ycroissant <yves.croissant@univ-reunion.fr>
2023-09-29 15:40:41 +0400

    remove the tikz directory

--- chapters/tikz/make.R
+++
@@ -1,8 +0,0 @@
-files <- c("OLS2D", "vectors2D", "OLS3D", "frishWaugh")
-for (i in files){
-    Sweave(paste("./Rnw/", i, ".Rnw", sep = "")) ;
-    system(paste("pdflatex ", i, ".tex", sep = ""))
-    system(paste("convert -density 600 ", i, ".pdf ", i, ".png", sep = ""))
-    system(paste("rm ", paste(i, c("aux", "log", "tex"), sep = ".", collapse = " ")))
-    system(paste("mv ", i, ".pdf ", i, ".png ./fig", sep = ""))
-}
